a418a5a4d4e0017a0194c8d6cca40a8acaa92a1e
jitesh1059 <25006745@siswa.um.edu.my>
2025-11-07 16:33:43 +0800

    done login integration

---
+++ ctrl/src/ctrl/Ctrl.java
@@ -0,0 +1,17 @@
+package ctrl;
+
+/**
+ *
+ * @author ctrl-c-ctrl-v
+ */
+
+// Imports setup
+import registration.LoginSystem;
+
+public class Ctrl {
+
+    public static void main(String[] args) {
+        // Step 1: Start login process
+        LoginSystem.start(); // This returns the logged-in User
+    }
+}
---
+++ ctrl/src/welcome/welcome.java
@@ -0,0 +1,44 @@
+package welcome;
+/**
+ *
+ * @author mingdao
+ */
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
+public class welcome {
+    
+    /**
+     * Landing page for user
+     * 
+     * @param username the user name that is supposed to be received from the text file
+     */
+    public static void welcome_user(String username) {
+        
+        // Take user name from login later
+        String name = username; // placeholder name
+        
+        ZoneId on9 = ZoneId.of("GMT+8");
+        LocalDateTime current = LocalDateTime.now(on9);
+        DateTimeFormatter f = DateTimeFormatter.ofPattern("'It is now' EEEE, MMMM d, yyyy 'at' h:mm a'.'");
+        String formattedCurrent = current.format(f);
+        System.out.println(formattedCurrent);
+        LocalTime morning = LocalTime.of(00, 0);
+        LocalTime afternoon = LocalTime.of(12, 0);
+        LocalTime evening = LocalTime.of(17, 0);
+
+        if (!current.toLocalTime().isAfter(morning) && current.toLocalTime().isBefore(afternoon)) {
+            System.out.printf("Good morning, %s!", name);
+
+        } else if (!current.toLocalTime().isAfter(afternoon) && current.toLocalTime().isBefore(evening)) {
+            System.out.printf("Good afternoon, %s!", name);
+
+        } else {
+            System.out.printf("Good evening, %s!", name);
+        }
+
+    }
+}
